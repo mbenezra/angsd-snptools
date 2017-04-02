0f47d3996e7640eef722653cfe8a7d4cf992e0a1
Michael Benezra <mbenezra@gmail.com>
2017-04-02 12:29:36 +0200

    updated

--- hg19.sh
+++ hg19.sh
@@ -6,7 +6,9 @@ wget --timestamping 'http://hgdownload.cse.ucsc.edu/admin/exe/linux.x86_64/twoBi
 wget --timestamping 'ftp://hgdownload.cse.ucsc.edu/goldenPath/hg19/bigZips/hg19.2bit' -O ./hg19/hg19.2bit
 
 chmod +x ./hg19/twoBitToFa
-./hg19/twoBitToFa .hg19/hg19.2bit ./hg19/hg19.fa
+./hg19/twoBitToFa ./hg19/hg19.2bit ./hg19/hg19.fa
+bgzip -c ./hg19/hg19.fa > ./hg19/hg19.fa.gz
+samtools faidx ./hg19.fa.gz
 
 for number in {1..22}; do echo chr$number; done > ./hg19/chr.list
 for chr in `cat ./hg19/chr.list`; do ./hg19/twoBitToFa ./hg19/hg19.2bit ./hg19/$chr.hg19.fa -seq=$chr; done
--- snptools.sh
+++ snptools.sh
@@ -2,12 +2,12 @@
 
 mkdir snptools
 
-for number in {1..22}; do echo angsd -sites ./hapmap/sites.txt -vcf-GL ./prob2vcf/out.vcf.gz -nInd 90 -SNP_pval 0.001 -doMajorMinor 1 -r $number: -doGeno 18 -doPost 2 -out snptools/snptools.uni.chr$number -fai ./hg19/chr$number.hg19.fa.gz.fai -doMaf 1; done > snptools.uni.txt
+for number in {1..22}; do echo angsd -sites ./hapmap/sites.txt -vcf-GL ./prob2vcf/out.vcf.gz -nInd 90 -SNP_pval 0.001 -doMajorMinor 1 -r $number: -doGeno 18 -doPost 2 -out snptools/snptools.uni.chr$number -fai ./hg19/hg19.fa.gz.fai -doMaf 1; done > snptools.uni.txt
 
 cat snptools.uni.txt | parallel
 
 wait
 
-for number in {1..22}; do echo angsd -sites sites.txt -vcf-GL out.vcf.gz -nInd 90 -SNP_pval 0.001 -doMajorMinor 1 -r $number: -doGeno 18 -doPost 1 -out snptools.freq.chr$number -fai ./hg19/chr$number.fa.gz.fai -doMaf 1; done > snptools.freq.txt
+for number in {1..22}; do echo angsd -sites ./hapmap/sites.txt -vcf-GL ./prob2vcf/out.vcf.gz -nInd 90 -SNP_pval 0.001 -doMajorMinor 1 -r $number: -doGeno 18 -doPost 1 -out snptools/snptools.freq.chr$number -fai ./hg19/hg19.fa.gz.fai -doMaf 1; done > snptools.freq.txt
 
 cat snptools.freq.txt | parallel
