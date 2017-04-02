2aea88eeb40f1bb50fa84c48b82bbc6cb107fbfa
Michael Benezra <mbenezra@gmail.com>
2017-04-02 11:56:37 +0200

    updated

--- snptools.sh
+++ snptools.sh
@@ -2,7 +2,7 @@
 
 mkdir snptools
 
-for number in {1..22}; do echo angsd -sites ./hapmap/sites.txt -vcf-GL ./prob2vcf/out.vcf.gz -nInd 90 -SNP_pval 0.001 -doMajorMinor 1 -r $number: -doGeno 18 -doPost 2 -out snptools/snptools.uni.chr$number -fai ./hg19/chr$number.fa.gz.fai -doMaf 1; done > snptools.uni.txt
+for number in {1..22}; do echo angsd -sites ./hapmap/sites.txt -vcf-GL ./prob2vcf/out.vcf.gz -nInd 90 -SNP_pval 0.001 -doMajorMinor 1 -r $number: -doGeno 18 -doPost 2 -out snptools/snptools.uni.chr$number -fai ./hg19/chr$number.hg19.fa.gz.fai -doMaf 1; done > snptools.uni.txt
 
 cat snptools.uni.txt | parallel
 
