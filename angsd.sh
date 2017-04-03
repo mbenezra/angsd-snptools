5e519588b6159fce63c1e96039cc74d05c2913ae
Michael Benezra <mbenezra@gmail.com>
2017-04-03 17:16:42 +0200

    updated

--- angsd.sh
+++ angsd.sh
@@ -6,10 +6,10 @@ angsd -sites ./hapmap/sites.txt -bam bams.list -SNP_pval 0.001 -doMaf 2 -doMajor
 
 mkdir angsd
 
-for number in {1..22}; do echo angsd -sites ./hapmap/sites.txt -bam bams.list -SNP_pval 0.001 -doMaf 2 -doMajorMinor 1 -r $number: -doGeno 18 -doPost 2 -GL 1 -out angsd/angsd.uni.chr$number; done > angsd.uni.txt
+for number in {1..22}; do echo angsd -sites ./hapmap/$number.sites.txt -bam bams.list -SNP_pval 0.001 -doMaf 2 -doMajorMinor 1 -r $number: -doGeno 18 -doPost 2 -GL 1 -out angsd/angsd.uni.chr$number; done > angsd.uni.txt
 
 cat angsd.uni.txt | parallel
 
-for number in {1..22}; do echo angsd -sites ./hapmap/sites.txt -bam bams.list -SNP_pval 0.001 -doMaf 2 -doMajorMinor 1 -r $number: -doGeno 18 -doPost 1 -GL 1 -out angsd/angsd.freq.chr$number; done > angsd.freq.txt
+for number in {1..22}; do echo angsd -sites ./hapmap/$number.sites.txt -bam bams.list -SNP_pval 0.001 -doMaf 2 -doMajorMinor 1 -r $number: -doGeno 18 -doPost 1 -GL 1 -out angsd/angsd.freq.chr$number; done > angsd.freq.txt
 
 cat angsd.freq.txt | parallel
--- gatk.sh
+++ gatk.sh
@@ -6,10 +6,10 @@ angsd -sites ./hapmap/sites.txt -bam bams.list -SNP_pval 0.001 -doMaf 2 -doMajor
 
 mkdir gatk
 
-for number in {1..22}; do echo angsd -sites ./hapmap/sites.txt -bam bams.list -SNP_pval 0.001 -doMaf 2 -doMajorMinor 1 -r $number: -doGeno 18 -doPost 2 -GL 2 -out gatk/gatk.uni.chr$number; done > gatk.uni.txt
+for number in {1..22}; do echo angsd -sites ./hapmap/$number.sites.txt -bam bams.list -SNP_pval 0.001 -doMaf 2 -doMajorMinor 1 -r $number: -doGeno 18 -doPost 2 -GL 2 -out gatk/gatk.uni.chr$number; done > gatk.uni.txt
 
 cat gatk.uni.txt | parallel
 
-for number in {1..22}; do echo angsd -sites ./hapmap/sites.txt -bam bams.list -SNP_pval 0.001 -doMaf 2 -doMajorMinor 1 -r $number: -doGeno 18 -doPost 1 -GL 2 -out gatk/gatk.freq.chr$number; done > gatk.freq.txt
+for number in {1..22}; do echo angsd -sites ./hapmap/$number.sites.txt -bam bams.list -SNP_pval 0.001 -doMaf 2 -doMajorMinor 1 -r $number: -doGeno 18 -doPost 1 -GL 2 -out gatk/gatk.freq.chr$number; done > gatk.freq.txt
 
 cat gatk.freq.txt | parallel
