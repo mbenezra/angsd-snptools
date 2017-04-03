9d2ee761af53c6940e2d90135ce8723482a1e24c
Michael Benezra <mbenezra@gmail.com>
2017-04-03 14:30:46 +0200

    updated

--- hg19.sh
+++ hg19.sh
@@ -8,7 +8,9 @@ wget --timestamping 'ftp://hgdownload.cse.ucsc.edu/goldenPath/hg19/bigZips/hg19.
 chmod +x ./hg19/twoBitToFa
 ./hg19/twoBitToFa ./hg19/hg19.2bit ./hg19/hg19.fa
 bgzip -c ./hg19/hg19.fa > ./hg19/hg19.fa.gz
-samtools faidx ./hg19.fa.gz
+samtools faidx ./hg19/hg19.fa.gz
+less -S ./hg19/hg19.fa.gz.fai | cut -c 4- > ./hg19/hg19.tmp
+mv ./hg19/hg19.tmp ./hg19/hg19.fa.gz.fai
 
 for number in {1..22}; do echo chr$number; done > ./hg19/chr.list
 for chr in `cat ./hg19/chr.list`; do ./hg19/twoBitToFa ./hg19/hg19.2bit ./hg19/$chr.hg19.fa -seq=$chr; done
