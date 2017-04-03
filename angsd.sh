e83865c2bdbf0ca846fed35a4432d4e64e3d541d
Michael Benezra <mbenezra@gmail.com>
2017-04-03 15:51:16 +0200

    updated

--- angsd.sh
+++ angsd.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+angsd -sites ./hapmap/sites.txt -bam bams.list -SNP_pval 0.001 -doMaf 2 -doMajorMinor 1 -doGeno 18 -doPost 2 -GL 1 -out angsd/angsd.uni
+
+angsd -sites ./hapmap/sites.txt -bam bams.list -SNP_pval 0.001 -doMaf 2 -doMajorMinor 1 -doGeno 18 -doPost 1 -GL 1 -out angsd/angsd.freq
+
 mkdir angsd
 
 for number in {1..22}; do echo angsd -sites ./hapmap/sites.txt -bam bams.list -SNP_pval 0.001 -doMaf 2 -doMajorMinor 1 -r $number: -doGeno 18 -doPost 2 -GL 1 -out angsd/angsd.uni.chr$number; done > angsd.uni.txt
--- snptools.sh
+++ snptools.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+angsd -sites ./hapmap/sites.txt -vcf-GL ./prob2vcf/out.vcf.gz -nInd 90 -SNP_pval 0.001 -doMajorMinor 1 -doGeno 18 -doPost 2 -out snptools/snptools.uni -fai ./hg19/hg19.fa.gz.fai -doMaf 1
+
+angsd -sites ./hapmap/sites.txt -vcf-GL ./prob2vcf/out.vcf.gz -nInd 90 -SNP_pval 0.001 -doMajorMinor 1 -doGeno 18 -doPost 1 -out snptools/snptools.freq -fai ./hg19/hg19.fa.gz.fai -doMaf 1
+
 mkdir snptools
 
 for number in {1..22}; do echo angsd -sites ./hapmap/sites.txt -vcf-GL ./prob2vcf/out.vcf.gz -nInd 90 -SNP_pval 0.001 -doMajorMinor 1 -r $number: -doGeno 18 -doPost 2 -out snptools/snptools.uni.chr$number -fai ./hg19/hg19.fa.gz.fai -doMaf 1; done > snptools.uni.txt
