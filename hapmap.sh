84acea625327c8220fe1e1b7d7446bd19a3c7fe7
Michael Benezra <mbenezra@gmail.com>
2017-04-04 16:32:17 +0200

    updated

--- hapmap.sh
+++ hapmap.sh
@@ -1,18 +1,41 @@
 #!/bin/sh
 
+# COPY HAPMAP
 mkdir hapmap
-
-for number in {1..22}; do vcftools --gzvcf ./hapmap/hapmap_3.3.hg19.vcf.gz --keep indv.txt --chr chr$number --recode --out ./hapmap/hapmap_3.3.ceu.chr$number.hg19; done
-
-for number in {1..22}; do gzip --keep ./hapmap/hapmap_3.3.ceu.chr$number.hg19.recode.vcf; done
-
 cp /storage/benezra/hapmap/hapmap_3.3.hg19.vcf.gz ./hapmap/.
-vcftools --gzvcf ./hapmap/hapmap_3.3.hg19.vcf.gz --keep indv.txt --not-chr chrX --recode --out ./hapmap/hapmap_3.3.ceu.hg19
-gzip --keep ./hapmap/hapmap_3.3.ceu.hg19.recode.vcf
- 
-for number in {1..22}; do grep -P "^chr$number\t" ./hapmap/hapmap_3.3.ceu.hg19.recode.vcf | awk '{print $1"\t"$2}' | cut -c 4- > ./hapmap/$number.sites.txt; done
+
+# 1..22 in 012 format
+for number in {1..22}; do vcftools --gzvcf ./hapmap/hapmap_3.3.hg19.vcf.gz --012 --max-alleles 2 --keep indv.txt --chr chr$number --out ./hapmap/hapmap_3.3.CEU.chr$number.hg19; done
+
+for number in {1..22}; do less -S ./hapmap/hapmap_3.3.CEU.chr$number.hg19.012.pos | cut -c 4- > ./hapmap/$number.sites.txt; done
 
 for number in {1..22}; do angsd sites index ./hapmap/$number.sites.txt; done
 
-grep "^[^#;]" ./hapmap/hapmap_3.3.ceu.hg19.recode.vcf | awk '{print $1"\t"$2}' | cut -c 4- > ./hapmap/sites.txt
+# one file in 012 format
+vcftools --gzvcf ./hapmap/hapmap_3.3.hg19.vcf.gz --012 --max-alleles 2 --keep indv.txt --not-chr chrX 
+--out ./hapmap/hapmap_3.3.CEU.hg19
+
+less -S ./hapmap/hapmap_3.3.CEU.hg19.012.pos | cut -c 4- > ./hapmap/sites.txt
+
 angsd sites index ./hapmap/sites.txt
+
+# 1..22 in recode format
+for number in {1..22}; do vcftools --gzvcf ./hapmap/hapmap_3.3.hg19.vcf.gz --keep indv.txt --chr chr$number --max-alleles 2 --recode --out ./hapmap/hapmap_3.3.CEU.chr$number.hg19; done
+
+for number in {1..22}; do gzip --keep ./hapmap/hapmap_3.3.CEU.chr$number.hg19.recode.vcf; done
+
+# one file in recode format
+vcftools --gzvcf ./hapmap/hapmap_3.3.hg19.vcf.gz --keep indv.txt --not-chr chrX --max-alleles 2 --recode --out ./hapmap/hapmap_3.3.CEU.hg19
+
+gzip --keep ./hapmap/hapmap_3.3.CEU.hg19.recode.vcf
+
+grep "^[^#;]" ./hapmap/hapmap_3.3.CEU.hg19.recode.vcf | awk '{print $1"\t"$2}' | cut -c 4- > ./hapmap/sites.txt
+angsd sites index ./hapmap/sites.txt
+
+# the rest 
+#for number in {1..22}; do grep -P "^chr$number\t" ./hapmap/hapmap_3.3.ceu.hg19.recode.vcf | awk '{print $1"\t"$2}' | cut -c 4- > ./hapmap/$number.sites.txt; done
+
+#for number in {1..22}; do angsd sites index ./hapmap/$number.sites.txt; done
+
+#grep "^[^#;]" ./hapmap/hapmap_3.3.ceu.hg19.recode.vcf | awk '{print $1"\t"$2}' | cut -c 4- > ./hapmap/sites.txt
+#angsd sites index ./hapmap/sites.txt
