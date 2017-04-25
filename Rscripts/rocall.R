7ec6e42af5837ded0f501a012a5053d85c9dd421
Michael Benezra <mbenezra@gmail.com>
2017-04-25 17:53:18 +0200

    updated

--- Rscripts/rocall.R
+++ Rscripts/rocall.R
@@ -21,7 +21,7 @@ roc <- function(true, gt, gl, pos){
 }
 
 loadIt <- function(file, indv) {
-  it <- read.table(file) #[c(1:1000),]
+  it <- read.table(file)[c(1:1000),]
   row.names(it) <- paste(it[,1], it[,2], sep = "_")
   it <- it[,-c(1,2)]
   names(it) <- indv
@@ -64,13 +64,17 @@ angsd.freq.roc <- roc(hapmap.hg19, angsd.freq.gt, angsd.freq.gl, pos)
 gatk.uni.roc <- roc(hapmap.hg19, gatk.uni.gt, gatk.uni.gl, pos)
 gatk.freq.roc <- roc(hapmap.hg19, gatk.freq.gt, gatk.freq.gl, pos)
 
-plot(snptools.uni.roc$spe, snptools.uni.roc$sen, main = "ROC curve - SNPTOOLS vs SAMTOOLS vs GATK", xlab = "Specificity", ylab = "Sensitivity", col = 1, type = "l", xlim = c(0, 1), ylim = c(0, 1))
-lines(snptools.freq.roc$spe, snptools.freq.roc$sen, col = 2, type = "l")
+size <- length(snptools.uni.roc$spe)
+samp <- sort(sample(size,floor(size/100), replace = F))
 
-lines(angsd.uni.roc$spe, angsd.uni.roc$sen, col = 3, type = "l")
-lines(angsd.freq.roc$spe, angsd.freq.roc$sen, col = 4, type = "l")
+plot(snptools.uni.roc$spe[samp], snptools.uni.roc$sen[samp], main = "ROC curve - SNPTOOLS vs SAMTOOLS vs GATK", xlab = "Specificity", ylab = "Sensitivity", col = 1, type = "l", xlim = c(0, 1), ylim = c(0, 1))
+lines(snptools.freq.roc$spe[samp], snptools.freq.roc$sen[samp], col = 2, type = "l")
 
-lines(gatk.uni.roc$spe, gatk.uni.roc$sen, col = 5, type = "l")
-lines(gatk.freq.roc$spe, gatk.freq.roc$sen, col = 6, type = "l")
+lines(angsd.uni.roc$spe[samp], angsd.uni.roc$sen[samp], col = 3, type = "l")
+lines(angsd.freq.roc$spe[samp], angsd.freq.roc$sen[samp], col = 4, type = "l")
+
+lines(gatk.uni.roc$spe[samp], gatk.uni.roc$sen[samp], col = 5, type = "l")
+lines(gatk.freq.roc$spe[samp], gatk.freq.roc$sen[samp], col = 6, type = "l")
 
 legend("topleft", c("SNPTOOLS uniform", "SNPTOOLS allele freq", "SAMTOOLS uniform", "SAMTOOLS allele freq", "GATK uniform", "GATK allele freq"), lwd = c(1,1,1,1,1,1), lty = c(1,1,1,1,1,1), col=c(1,2,3,4,5,6))
+
